a2d270572f313e7fbe013cfeb0ec14057aff9be0
AdamKral97 <31903140+AdamKral97@users.noreply.github.com>
2019-04-13 16:50:40 -0500

    profile stuff

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:profile_id>/', views.profile_view, name = "profile-view")
+    path('<int:profile_id>/', views.profile_view, name = "profile-view"),
+    path("create-profile", views.create_profile, name = "create-profile")
 ]
