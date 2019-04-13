275178933540c65c9437327794acf264aeb8dd36
knospe <willk4898@gmail.com>
2019-04-13 18:34:53 -0500

    Search thingy

--- organizations/urls.py
+++ organizations/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('organization-view/<int:organization_id>/<slug:tab>/', views.organization_view, name='organization-view-tab'),
     path('organization-join/<int:organization_id>/<slug:tab>/', views.organization_join, name='organization-join'),
     path('organization-leave/<int:organization_id>/<slug:tab>/', views.organization_leave, name='organization-leave'),
-    path('organization-create/', views.create_organization, name='organization-create')
+    path('organization-create/', views.create_organization, name='organization-create'),
+    path('get-organizations-by-name/', views.get_organizations_by_name, name='get-organizations-by-name')
 ]
---
+++ static/javascript/search.js
@@ -0,0 +1,1 @@
+// todo: probably this should be where the search is
